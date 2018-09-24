791efc36c01dfa394b8a358c82c8ff13e80b89a4
Elias <elias2603@hotmail.com>
2018-09-24 23:56:02 +0200

    Naive Bayes and SVM

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+### import the sklearn module for GaussianNB
+from sklearn.naive_bayes import GaussianNB
 
+#Importing the module to use accurancy_score
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -26,8 +30,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+def trainingNB(features_train, labels_train, features_test, labels_test):
+    clf= GaussianNB()
+
+    #Let's measure how much time takes the training
+    t0=time()
+    clf.fit(features_train, labels_train)
+    print("training time: ",round(time()-t0,3),"s")
+
+    t1=time()
+    predictions=clf.predict(features_test)
+    print("predict time: ",round(time()-t1,3),"s")
+
+    accuracy=accuracy_score(labels_test,predictions)
+    return accuracy
 
 
 #########################################################
 
+#MAIN
 
+print(trainingNB(features_train,labels_train,features_test,labels_test))
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -26,5 +26,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
+### import the sklearn module for SVM
+from sklearn.svm import SVC
 
+#Importing the module to use accurancy_score
+from sklearn.metrics import accuracy_score
 
+from collections import Counter
+
+clf= SVC(kernel='rbf', C=10000.0)
+
+t0=time()
+print("Start fitting")
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+clf.fit(features_train, labels_train)
+print("training time: ",round(time()-t0,3),"s")
+
+t1=time()
+predictions=clf.predict(features_test)
+print("predict time: ",round(time()-t1,3),"s")
+
+accuracy=accuracy_score(labels_test,predictions)
+print(accuracy)
+print (Counter(predictions))
