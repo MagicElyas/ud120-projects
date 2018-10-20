4d35ac2af5115f176989daf764836ebf0bc2a56f
Elias <MagicElyas@users.noreply.github.com>
2018-10-20 15:55:04 +0200

    Lesson 9 completed :)

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
-""" 
+"""
     Skeleton code for k-means clustering mini-project.
 """
 
@@ -40,31 +40,68 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 
 ### load in the dict of dicts containing all the data on each person in the dataset
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
-### there's an outlier--remove it! 
+### there's an outlier--remove it!
 data_dict.pop("TOTAL", 0)
 
 
-### the input features we want to use 
-### can be any key in the person-level dictionary (salary, director_fees, etc.) 
+### the input features we want to use
+### can be any key in the person-level dictionary (salary, director_fees, etc.)
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
 
 ### in the "clustering with 3 features" part of the mini-project,
-### you'll want to change this line to 
+### you'll want to change this line to
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+for f1, f2,f3 in finance_features:
+    plt.scatter( f1, f3 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+from sklearn.cluster import KMeans
+from sys import maxint
+
+kmeans=KMeans(n_clusters=2)
+kmeans.fit(finance_features)
+pred=kmeans.labels_
+
+
+
+# Let's stract the min and max values of exercised_stock_options
+min=maxint
+max=-1
+evaluatedft='exercised_stock_options'
+
+# evaluating exercised_stock_options
+for i in data_dict.values():
+    if(i[evaluatedft]!= 'NaN'):
+        if(i[evaluatedft] < min): min= i[evaluatedft]
+        if(i[evaluatedft] > max): max= i[evaluatedft]
+
+
+print('MIN exercised_stock_options: '+str(min))
+print('MAX exercised_stock_options: '+str(max))
+
+
+evaluatedft='salary'
+min=maxint
+max=0
+# Evaluating salary
+for i in data_dict.values():
+    if(i[evaluatedft]!= 'NaN'):
+        if(i[evaluatedft] < min): min= i[evaluatedft]
+        if(i[evaluatedft] > max): max= i[evaluatedft]
+
+print('MIN salary: '+str(min))
+print('MAX salary: '+str(max))
 
 
 
